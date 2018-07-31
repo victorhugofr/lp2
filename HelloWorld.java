24fa42117b87389a4545fecfbf8a90c11ef10954
Victor Hugo Freire Ramalho <vale1c@A305-32L.imd.local>
2018-07-31 08:26:34 -0300

    first

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Ola Mundo");
+		System.out.println("eaeee");
+		int x = 2;
+		System.out.println("Ola %x"+x);
+	}
+}
