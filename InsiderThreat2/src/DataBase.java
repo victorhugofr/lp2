fb29ff050f4df101060f7c0b5e871c39ea33e773
Victor Hugo <vale1c@ufrn.edu.br>
2018-11-07 19:07:55 -0300

    funcionando!

--- InsiderThreat2/src/DataBase.java
+++ InsiderThreat2/src/DataBase.java
@@ -10,37 +10,33 @@ public class DataBase {
 	public void verifica(Action teste) {
 		Node aux;
 		Node aux2;
+		Node aux3;
 		boolean existe= false;
 		for(int i=0;i<users.size();i++) {
 		//	System.out.println(teste.getUser());
-			aux=users.get(i).existe(users.get(i).getRoot(),teste.getUser());
+			aux=users.get(i).existe(users.get(i).getRoot(),teste.getUser()); 
 			if(aux!=null) {//procura usuario
 				existe=true;
-				System.out.println(existe);
 				aux.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-				aux=users.get(i).existe(aux,teste.getPc());
-				if(aux!=null) {//procura PC
+				aux2=users.get(i).existe(aux,teste.getPc()); 
+				if(aux2!=null) {//procura PC
 					aux.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-					aux2=users.get(i).existe(aux,teste.getActivity());
-					if(aux2!=null) {//procura atividade
-						aux2.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
+					aux3=users.get(i).existe(aux,teste.getActivity());
+					if(aux3!=null) {//procura atividade
+						aux3.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
 					}else {
 						Node activity=new Node(teste.getActivity());
 						activity.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-						aux.getChilds().add(activity);
+						aux2.getChilds().add(activity);
 					}
 				}else {
 					Node PC= new Node(teste.getPc());
 					Node activity=new Node(teste.getActivity());
 					PC.getChilds().add(activity);
 					PC.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-					users.get(i).existe(users.get(i).getRoot(),teste.getUser()).getChilds().add(PC);
+					aux.getChilds().add(PC);
 				}
 			}
-			/**if(users.get(i).root.value==teste[2]) {
-				users.get(i).root.hist[Integer.parseInt(teste[1].substring(11, 13))]++;
-				existe=true;
-			}*/
 		}
 		
 		if(existe==false) {
@@ -61,9 +57,21 @@ public class DataBase {
 			users.add(user);
 		}
 	}
+	
 	public void print() {
-		for(int i=0;i<users.size();i++) {
-			System.out.println(users.get(i).getRoot().getValue());
+		Node aux,aux2,aux3;
+		int i,j,z;
+		for(i=0;i<users.size();i++) {
+			aux=users.get(i).getRoot();
+			System.out.println(aux.getValue());
+			for(z=0;z<aux.getChilds().size();z++) {
+				aux2=aux.getChildAt(z);
+				System.out.println(aux2.getValue());
+				for(j=0;j<aux2.getChilds().size();j++) {
+					aux3=aux2.getChildAt(j);
+					System.out.println(aux3.getValue());
+				}
+			}
 		}
 	}
 }
--- InsiderThreat2/src/Main.java
+++ InsiderThreat2/src/Main.java
@@ -6,8 +6,10 @@ public class Main {
 		Leitor arquivo = new Leitor();
 		DataBase db=new DataBase();
 		String[] teste;
+		
 		try {
 			Scanner leitor = new Scanner(arquivo.leitura("src/device2.csv"));
+			
 			String linha = new String();
 			leitor.nextLine();
 			while(leitor.hasNext()){
@@ -20,6 +22,15 @@ public class Main {
 				
 					
 				//System.out.println(Arrays.toString(teste));		
+			}
+			Scanner leitor2 = new Scanner(arquivo.leitura("src/logon2.csv"));
+			leitor2.nextLine();
+			while(leitor2.hasNext()){
+				linha = leitor2.nextLine();
+				arquivo.setValoresEntreVirgulas(linha);
+				teste= arquivo.quebrandoLinhas();
+				Logon lo2=new Logon(teste[0],teste[1],teste[2],teste[3],teste[4]);
+				db.verifica(lo2);	
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
